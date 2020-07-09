ed1efbdefaf164b0a6d873c434c768003ae6720d
Valentin-Varuk <valuna5505@gmail.com>
2020-07-09 21:43:41 +0300

    update view home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged fucking in!
                 </div>
             </div>
         </div>
